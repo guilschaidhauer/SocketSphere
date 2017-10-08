f22ce0c4e8f22126cafecb04c3dfed091fcb7d08
guilschaidhauer <guilherme.schaidhauer.de.castro@sap.com>
2017-10-08 20:08:36 -0300

    Clean up code

--- Sockets/Source.cpp
+++ Sockets/Source.cpp
@@ -74,12 +74,12 @@ void Init()
 	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
 	iLowH = 0;
-	iHighH = 159;
+	iHighH = 140;
 
-	iLowS = 45;
+	iLowS = 35;
 	iHighS = 255;
 
-	iLowV = 155;
+	iLowV = 154;
 	iHighV = 255;
 
 	//Create trackbars in "Control" window
@@ -138,7 +138,7 @@ void Detect()
 		double dM10 = oMoments.m10;
 		double dArea = oMoments.m00;
 
-		//imshow("Thresholded Image", imgThresholded); //show the thresholded image
+		imshow("Thresholded Image", imgThresholded); //show the thresholded image
 
 		vector<vector<Point> > contours;
 		vector<Vec4i> hierarchy;
@@ -217,81 +217,6 @@ void parseString(string s, Circle *circle)
 	circle->Radius = std::stof(s, nullptr);
 }
 
-void runServer()
-{
-	/*SOCKET s;
-	struct sockaddr_in server, si_other;
-	int slen, recv_len;
-	char buf[BUFLEN];
-	WSADATA wsa;
-
-	slen = sizeof(si_other);
-
-	//Initialise winsock
-	printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-	printf("Failed. Error Code : %d", WSAGetLastError());
-	exit(EXIT_FAILURE);
-	}
-	printf("Initialised.\n");
-
-	//Create a socket
-	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
-	{
-	printf("Could not create socket : %d", WSAGetLastError());
-	}
-	printf("Socket created.\n");
-
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(PORT);
-
-	//Bind
-	if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
-	{
-	printf("Bind failed with error code : %d", WSAGetLastError());
-	exit(EXIT_FAILURE);
-	}
-	puts("Bind done");
-
-	//keep listening for data
-	while (1)
-	{
-	printf("Waiting for data...");
-	fflush(stdout);
-
-	//clear the buffer by filling null, it might have previously received data
-	memset(buf, '\0', BUFLEN);
-
-	//try to receive some data, this is a blocking call
-	if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
-	{
-	printf("recvfrom() failed with error code : %d", WSAGetLastError());
-	exit(EXIT_FAILURE);
-	}
-
-	//print details of the client/peer and the data received
-	printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-	//printf("Data: %s\n", buf);
-
-	string str(buf);
-	cout << "hehe:" << str << endl;
-
-
-	//now reply the client with the same data
-	if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
-	{
-	printf("sendto() failed with error code : %d", WSAGetLastError());
-	exit(EXIT_FAILURE);
-	}
-	}
-
-	closesocket(s);
-	WSACleanup();*/
-}
-
 void runClient()
 {
 	struct sockaddr_in si_other;
@@ -363,17 +288,5 @@ int main()
 	//Detect();
 	runClient();
 
-	/*cin >> connectionType;
-
-	if (connectionType == "s")
-	{
-	runServer();
-	}
-	else if (connectionType == "c")
-	{
-	runClient();
-	}*/
-
-	//system("pause");
 	return 0;
 }
